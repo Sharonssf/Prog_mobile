385951c6103a463832ce06ba63851bfaa3ef64ac
Samuel <samuel.sharon027@gmail.com>
2024-06-21 16:32:48 -0300

    login feito

--- app/src/main/java/com/example/barbershop4/MainActivity.kt
+++ app/src/main/java/com/example/barbershop4/MainActivity.kt
@@ -1,20 +1,66 @@
 package com.example.barbershop4
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.barbershop4.databinding.ActivityMainBinding
+import com.example.barbershop4.view.Home
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.btLogin.setOnClickListener{
+
+            val nome = binding.editNome.text.toString()
+            val senha = binding.editSenha.text.toString()
+
+            when{
+                nome.isEmpty() -> {
+                    mensagem(it, "Primeiro tem que colocar o nome ne coisa linda")
+                }
+                senha.isEmpty() -> {
+                    mensagem(it, "Sem senha também não da ne coisa fofa")
+                }
+                senha.length < 6 ->{
+                    mensagem(it, "Menos de 6 caracteres ta pedindo para ser hackeado né")
+                }
+                else ->{
+                    navegarPraHone(nome)
+                }
+            }
+        }
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+            }
         }
+
+    private fun mensagem(view: View, mensagem: String){
+        val snackbar = Snackbar.make(view, mensagem, Snackbar.LENGTH_SHORT)
+        snackbar.setBackgroundTint(Color.parseColor("#FF0000"))
+        snackbar.setTextColor(Color.parseColor("#FFFFFF"))
+        snackbar.show()
+    }
+
+    private fun navegarPraHone(nome: String){
+        val intent = Intent(this, Home::class.java)
+        intent.putExtra("nome", nome)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/barbershop4/view/Home.kt
+++ app/src/main/java/com/example/barbershop4/view/Home.kt
@@ -1,36 +1,16 @@
 package com.example.barbershop4.view
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.barbershop4.R
-import com.example.barbershop4.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
-
 class Home : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         supportActionBar?.hide()
-        binding.btLogin.setOnClickListener{
-
-            val nome = binding.editNome.text.toString()
-            val senha = binding.editSenha.text.toString()
-
-            when{
-                nome.isEmpty() -> {
-                    mensagem(it, mensagem = "Coloque o seu nome!")
-
-                }
-            }
-        }
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -38,12 +18,5 @@ class Home : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    private fun mensagem(view: View, mensagem: String){
-        val snackbar = Snackbar.make(view, mensagem, Snackbar.LENGTH_SHORT)
-        snackbar.setBackgroundTint(Color.parseColor(colorString:"#FF0000"))
-        snackbar.setTextColor(Color.parseColor(colorString:"#FFFFF"))
-        snackbar.show()
     }
 }
