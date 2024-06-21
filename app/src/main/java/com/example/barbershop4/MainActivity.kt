d1daa438ef0a3b35a788cb67031e2016cba557e8
Samuel <samuel.sharon027@gmail.com>
2024-06-21 17:05:30 -0300

    resolvendo bugs

--- app/src/main/java/com/example/barbershop4/MainActivity.kt
+++ app/src/main/java/com/example/barbershop4/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -15,6 +14,7 @@ import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -22,43 +22,41 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
-        binding.btLogin.setOnClickListener{
-
+        binding.btLogin.setOnClickListener {
             val nome = binding.editNome.text.toString()
             val senha = binding.editSenha.text.toString()
 
-            when{
+            when {
                 nome.isEmpty() -> {
-                    mensagem(it, "Primeiro tem que colocar o nome ne coisa linda")
+                    mensagem(binding.root, "Primeiro tem que colocar o nome né coisa linda")
                 }
                 senha.isEmpty() -> {
-                    mensagem(it, "Sem senha também não da ne coisa fofa")
+                    mensagem(binding.root, "Sem senha também não dá né coisa fofa")
                 }
-                senha.length < 6 ->{
-                    mensagem(it, "Menos de 6 caracteres ta pedindo para ser hackeado né")
+                senha.length < 6 -> {
+                    mensagem(binding.root, "Menos de 6 caracteres tá pedindo para ser hackeado né")
                 }
-                else ->{
-                    navegarPraHone(nome)
+                else -> {
+                    navegarPraHome(nome)
                 }
             }
         }
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-            }
         }
+    }
 
-    private fun mensagem(view: View, mensagem: String){
+    private fun mensagem(view: View, mensagem: String) {
         val snackbar = Snackbar.make(view, mensagem, Snackbar.LENGTH_SHORT)
         snackbar.setBackgroundTint(Color.parseColor("#FF0000"))
         snackbar.setTextColor(Color.parseColor("#FFFFFF"))
         snackbar.show()
     }
 
-    private fun navegarPraHone(nome: String){
+    private fun navegarPraHome(nome: String) {
         val intent = Intent(this, Home::class.java)
         intent.putExtra("nome", nome)
         startActivity(intent)
